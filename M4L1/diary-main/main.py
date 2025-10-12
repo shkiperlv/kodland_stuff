06759ca1bb6b49cdd0fbd96c6090139153587658
shkiperlv <87937161+shkiperlv@users.noreply.github.com>
2025-10-12 15:25:36 +0300

    Add files via upload

---
+++ M4L1/diary-main/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    subtitle = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Card {self.id}>'
+
+@app.route('/')
+def index():
+    cards = Card.query.order_by(Card.id).all() 
+    return render_template('index.html', cards=cards) 
+
+
+@app.route('/card/<int:id>')
+def card(id):
+    card = Card.query.get(id) 
+    return render_template('card.html', card=card)
+
+
+@app.route('/create')
+def create():
+    return render_template('create_card.html')
+
+
+@app.route('/form_create', methods=['GET', 'POST'])
+def form_create():
+    if request.method == 'POST':
+        title = request.form['title']
+        subtitle = request.form['subtitle']
+        text = request.form['text']
+
+       
+        card = Card(title=title, subtitle=subtitle, text=text)  
+        db.session.add(card)  
+        db.session.commit()   
+
+        return redirect('/')
+    else:
+        return render_template('create_card.html')
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  
+    app.run(debug=True)
